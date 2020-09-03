24c99d82ca37e1c371243d2e32525894f7af3d4a
Tyler Wolverton <tylerwolvert@gmail.com>
2020-09-03 15:22:31 -0500

    Revert "Merge remote-tracking branch 'upstream/master'"
    This reverts commit 66f316d4466dfaf21ef10e921e917920ce86deb2, reversing
    changes made to 9cda7ae4872ceab37b95add6a267d49478dea8bd.

--- Network/Network.cpp
+++ Network/Network.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Network.cpp : Defines the functions for the static library.
 //
 
@@ -39,48 +38,3 @@ void Network::Shutdown()
 }
 
 
-=======
-
-#include <Network.h>
-#include <exception>
-#include <strstream>
-
-void Network::initialize()
-{
-    // Initalize winsock
-    WSADATA wsaData;
-    WORD wVersion = MAKEWORD(2, 2);
-    int iResult = WSAStartup(wVersion, &wsaData);
-    if (iResult != 0)
-    {
-        std::ostrstream ostr;
-        ostr << "Winsock startup failed, error = " << iResult << std::ends;
-        throw std::runtime_error(ostr.str());
-    }
-}
-
-void Network::uninitialize()
-{
-    int iResult = WSACleanup();
-    if (iResult == SOCKET_ERROR)
-    {
-        std::ostrstream ostr;
-        ostr << "Winsock cleanup failed, error = " << WSAGetLastError() << std::ends;
-        throw std::runtime_error(ostr.str());
-    }
-}
-
-
-std::shared_ptr<TCPServer>& Network::createTCPServer(Mode mode)
-{
-    m_tcpServers.push_back(std::shared_ptr<TCPServer>(new TCPServer(mode)));
-    return m_tcpServers.back();
-}
-
-
-std::shared_ptr<TCPClient>& Network::createTCPClient()
-{
-    m_tcpClients.push_back(std::shared_ptr<TCPClient>(new TCPClient()));
-    return m_tcpClients.back();
-}
->>>>>>> upstream/master
--- Server/ServerMain.cpp
+++ Server/ServerMain.cpp
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
 #include <Network.hpp>
 #include <iostream>
 
-=======
-
-#include <Network.h>
-#include <iostream>
-
-#pragma comment(lib, "Ws2_32.lib")
-
->>>>>>> upstream/master
 int main()
 {
 	try
 	{
-<<<<<<< HEAD
 		std::unique_ptr<Network> network( new Network() );
 
 		network->Startup();
@@ -29,57 +19,3 @@ int main()
 		
 	}
 }
-=======
-		std::unique_ptr<Network> network(new Network());
-		network->initialize();
-
-		try
-		{
-			std::shared_ptr<TCPServer> server;
-			TCPSocket socket;
-			std::uint16_t portNumber = 48000;
-
-			server = network->createTCPServer(Mode::Blocking);
-			server->bind(portNumber);
-			server->listen();
-			std::cout << "Server listening or port " << portNumber << std::endl;
-			socket = server->accept();
-			std::cout << "Client connected from " << socket.getAddress() << std::endl;
-
-			TCPData buf;
-			do {
-				buf = socket.receive();
-				std::string msg(buf.getData(), buf.getLength());
-				if (buf.getLength() > 0)
-				{
-					std::cout << "Received message from client: " << msg << std::endl;
-				}
-				std::string data("Here is your data");
-				socket.send(&data[0], data.length());
-			} while (buf.getLength() > 0);
-			std::cout << "Client disconnecting..." << std::endl;
-			server->stopListen();
-			socket.shutdown();
-		}
-		catch(std::runtime_error& rt)
-		{
-			std::cerr << "Server error = " << rt.what() << std::endl;
-		}
-		catch (std::exception e)
-		{
-			std::cerr << "C++ exception = " << e.what() << std::endl;
-		}
-
-		network->uninitialize();
-	}
-	catch (std::runtime_error& rt)
-	{
-		std::cerr << "Server error = " << rt.what() << std::endl;
-	}
-	catch (std::exception e)
-	{
-		std::cerr << "C++ exception = " << e.what() << std::endl;
-	}
-	return 0;
-}
->>>>>>> upstream/master
